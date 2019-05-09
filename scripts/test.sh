65b027ece4dd665121fba17494dca8799728b27d
Justin Palmer <encytemedia@gmail.com>
2019-05-09 10:45:23 -0700

    see what docker sees

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,5 @@
 
 cd /usr/src/app
 echo "some_password: welcome" > secrets.yaml
-/usr/local/bin/python -m homeassistant --config ./ --script check_config --info all
+ls ./
+# /usr/local/bin/python -m homeassistant --config ./ --script check_config --info all
