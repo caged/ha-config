92e882266448a05b4220234ff64ae5de7c1f4dc6
Justin Palmer <encytemedia@gmail.com>
2019-05-09 10:59:22 -0700

    add a test secrets file

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /usr/src/app
-echo "some_password: welcome" > secrets.yaml
-# ls $GITHUB_WORKSPACE
+cp $GITHUB_WORKSPACE/secrets.test.yaml $GITHUB_WORKSPACE/secrets.yaml
 /usr/local/bin/python -m homeassistant --config $GITHUB_WORKSPACE --script check_config --info all
