31680871fa4bb7e18f3032813d52f2ee8e413c59
Justin Palmer <encytemedia@gmail.com>
2019-05-09 10:41:30 -0700

    relative target config dir

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 cd /usr/src/app
 echo "some_password: welcome" > secrets.yaml
-/usr/local/bin/python -m homeassistant --config / --script check_config --info all
+/usr/local/bin/python -m homeassistant --config ./ --script check_config --info all
