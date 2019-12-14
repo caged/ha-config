eb05fd091dde9883223c56f80e4f637bc7575b62
Justin Palmer <encytemedia@gmail.com>
2019-12-14 14:53:23 -0800

    remove unused config

--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /usr/src/app
-cp $GITHUB_WORKSPACE/secrets.test.yaml $GITHUB_WORKSPACE/secrets.yaml
-/usr/local/bin/python -m homeassistant --config $GITHUB_WORKSPACE --script check_config --info all
